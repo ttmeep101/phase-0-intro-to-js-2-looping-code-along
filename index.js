d261dc91565c6418a70c2707c79e71ec0fb6b6a7
Tristan Tritch <ttmeep101@hotmail.com>
2024-09-30 21:51:58 -0500

    completes tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let thankYouArray =[];
+
+function writeCards(names,occasion){
+    let num = 0;
+    const arrayLength = names.length;
+    while(num < arrayLength){
+        thankYouArray.push(`Thank you, ${names[num]}, for the wonderful ${occasion} gift!`);
+        num++;
+    }
+    return thankYouArray;
+}
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
